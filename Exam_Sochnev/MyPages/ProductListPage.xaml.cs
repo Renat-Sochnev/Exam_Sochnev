149144ea51dbe933ec018cb1156873d74a84fb19
User07 <User1020@User1020.User1020>
2024-07-04 10:06:46 +0300

    Добавьте файлы проекта.

---
+++ Exam_Sochnev/MyPages/ProductListPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam_Sochnev.MyPages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListPage.xaml
+    /// </summary>
+    public partial class ProductListPage : Page
+    {
+        public ProductListPage()
+        {
+            InitializeComponent();
+            List<int> list = new List<int>();
+            for(int x = 0; x < 7; x++)
+            {
+                list.Add(1);
+            }
+            Productlist.ItemsSource = list;
+        }
+    }
+}
